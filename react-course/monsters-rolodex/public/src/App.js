bfe60a479827fa8ff235f2b3b415b5ee694c4406
Steve Beville <steve_beville@hotmail.com>
2019-08-05 23:31:27 +0100

    test

---
+++ react-course/monsters-rolodex/public/src/App.js
@@ -0,0 +1,35 @@
+// jshint esversion:6
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type='search'
+          placeholder='Search monsters'
+          onChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
