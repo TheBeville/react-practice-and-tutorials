bbd8ef519e0baa382cd2247cbdcd17430a34ca69
Steve Beville <steve_beville@hotmail.com>
2019-08-07 18:46:02 +0100

    latest update

--- react-course/monsters-rolodex/src/App.js
+++ react-course/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 // jshint esversion:6
-import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -27,10 +28,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='Search monsters'
-          onChange={e => this.setState({searchField: e.target.value})} />
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
