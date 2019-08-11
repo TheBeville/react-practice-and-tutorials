27926aa71aab05ddd16a348e9939fde17c3b3417
Steve Beville <steve_beville@hotmail.com>
2019-08-11 21:29:51 +0100

    link sign-in component to Firebase authentication

--- react-course/e-commerce-project/src/components/sign-in/sign-in.component.jsx
+++ react-course/e-commerce-project/src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -18,10 +18,17 @@ class SignIn extends React.Component {
   }
 
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({email: '', password: ''});
+    const {email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''});
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   handleChange = event => {
@@ -33,7 +40,7 @@ class SignIn extends React.Component {
   render() {
     return(
       <div className='sign-in'>
-        <h2>I already have an account</h2>
+        <h2 className='title'>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
