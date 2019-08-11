e00f6ac2f1392ffa1c375d2cdd3ce6a87d7e6276
Steve Beville <steve_beville@hotmail.com>
2019-08-11 20:25:10 +0100

    add sign-up via email

--- react-course/e-commerce-project/src/firebase/firebase.utils.js
+++ react-course/e-commerce-project/src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
   appId: "1:292327594703:web:6b494477254d2623"
 };
 
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (err) {
+      console.log('error creating user', err.message);
+    }
+  }
+
+  return userRef;
+};
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
