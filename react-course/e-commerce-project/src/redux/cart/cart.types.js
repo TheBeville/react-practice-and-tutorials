c7cd5522d8f4e4d755ae4be303de4902d4350cdc
Steve Beville <steve_beville@hotmail.com>
2019-08-15 21:48:25 +0100

    add to cart functionality

--- react-course/e-commerce-project/src/redux/cart/cart.types.js
+++ react-course/e-commerce-project/src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 // jshint esversion:6
 const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
